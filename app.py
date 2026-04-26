b4354d031df8a33538ddf3e52f53a2ff206be865
Ayan Guha <ayanguha@Versent-Mac-JHXK7JD6TK.local>
2026-04-26 17:54:26 +1000

    Add basic save

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template,send_from_directory, request, jsonify
-import os 
+import os, json
 from hashlib import sha1
 import csv 
 
 
 app = Flask(__name__)
 
+ALL_QUESTION_METADATA_FILE = "static/metadata/questions.tsv"
 @app.route('/')
 def landing():
     return render_template('index.html')
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    metadata = generate_metadata(ALL_QUESTION_METADATA_FILE)
+    return render_template('home.html', metadata=metadata)
 
 @app.route('/history')
 def history():
@@ -37,6 +39,27 @@ def history():
 def __hash_func__(data: str) -> str:
     return sha1(data.encode()).hexdigest()
 
+def generate_metadata(file_path):
+    l = []
+    with open(file_path, mode='r', encoding='utf-8') as f:
+        reader = csv.DictReader(f, delimiter='\t')
+        for i, row in enumerate(reader, start=1):
+            focus = row['focus']
+            trait = row['care_trait']
+            sub_capabilities = row['care_sub_capability']
+            question_id = __hash_func__(f"{focus}_{trait}_{sub_capabilities}_{i}".replace(" ", "_"))
+            question_answered = safe_get_reposnse(question_id)['question_answered']
+            l.append({'question_id': question_id, 'question_answered': question_answered})
+        
+    total_questions = len(l) 
+    answered_questions = sum([1 for x in l if x['question_answered']])
+    pct_completed = round(100*(answered_questions/total_questions),2)
+    print(f"total_questions: {total_questions}, answered_questions: {answered_questions}, pct_completed:{pct_completed} ")
+
+
+    return {'total_questions': total_questions, 'answered_questions': answered_questions, 'pct_completed': pct_completed}
+        
+
 def generate_nested_list(file_path):
     nested_data = {}
     focus_id_map = {}
@@ -46,6 +69,7 @@ def generate_nested_list(file_path):
     with open(file_path, mode='r', encoding='utf-8') as f:
         reader = csv.DictReader(f, delimiter='\t')
         for i, row in enumerate(reader, start=1):
+
             focus = row['focus']
             trait = row['care_trait']
             sub_capabilities = row['care_sub_capability']
@@ -63,6 +87,7 @@ def generate_nested_list(file_path):
 
 
             question_id = __hash_func__(f"{focus}_{trait}_{sub_capabilities}_{i}".replace(" ", "_"))
+
 
             # Build nesting
             if area_id not in nested_data:
@@ -100,9 +125,7 @@ def generate_nested_list(file_path):
                 "id": trait_id,
                 "name": trait_id_map[trait_id],
                 "sub_capabilities": scs
-            })
-
-            
+            })            
 
         result.append(focus_entry)
         
@@ -112,14 +135,56 @@ def generate_nested_list(file_path):
 @app.route('/save_response', methods=['POST'])
 def save_response():
     data = request.get_json()
-    #question_id = data.get('question_id')
-    #evidence = data.get('evidence')
-    print(data)
+    question_id = data.get('question_id')
+    print(f"data: {data}")
+    if not os.path.exists("static/data/responses.json"):
+        stored_responses = {}
+    else:
+        stored_responses = json.load(open("static/data/responses.json"))
     
+    stored_responses[question_id] = data 
+    
+    with open('static/data/responses.json', 'w', encoding='utf-8') as f:
+        json.dump(stored_responses, f, ensure_ascii=False, indent=4)
+
     # Logic to save to a database, session, or file
     # Example: print(f"Saving {question_id}: {evidence}")
     
     return jsonify({"status": "success", "message": "Response saved"}), 200
+
+def safe_read_responses():
+    if not os.path.exists("static/data/responses.json"):
+        stored_responses = {}
+    else:
+        stored_responses = json.load(open("static/data/responses.json"))
+
+    return stored_responses
+
+def safe_get_reposnse(qid):
+    stored_responses = safe_read_responses()
+   
+    if qid in stored_responses.keys():
+        response_text = stored_responses[qid]['answer']
+        res = {"question_answered": True, "response": response_text}
+    else:
+        response_text = ''
+        res = {"question_answered": False, "response": response_text}
+    return res 
+
+
+@app.route("/get_response", methods=["POST"])
+def get_response():
+    qid = request.get_json().get('question_id')
+    try:
+        qa = safe_get_reposnse(qid) 
+        res = {"success": 'true',"qid": qid, "question_answered": qa['question_answered'], "response": qa['response']}
+        print(f"======== Response Text: {res}")
+        return jsonify(res)
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error": str(e)
+        }), 500
 
 @app.route('/reports/sample')
 def sample_report():
@@ -129,7 +194,7 @@ def sample_report():
 @app.route('/library')
 def library():
     '''knowledge_areas = '''
-    knowledge_areas = generate_nested_list("static/data/questions.tsv")
+    knowledge_areas = generate_nested_list(ALL_QUESTION_METADATA_FILE)
     return render_template('qa.html', areas=knowledge_areas)
 
 # Route to serve the docs
